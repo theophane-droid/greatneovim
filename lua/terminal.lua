0b1ad16bfd77f3b4f21e19563b4de1cec78987f0
theophane-droid <theodesile@gmail.com>
2025-07-10 16:26:58 +0200

    keep term size

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,14 +1,22 @@
-local term_buf, term_win
+local term_buf, term_win, term_height
 
--- Open term and create if not already exists
+vim.api.nvim_create_autocmd("WinResized", {
+  callback = function()
+    if term_win and vim.api.nvim_win_is_valid(term_win) then
+      term_height = vim.api.nvim_win_get_height(term_win)
+    end
+  end,
+})
+
 function ToggleTerminal()
   if term_win and vim.api.nvim_win_is_valid(term_win) then
-    if vim.api.nvim_get_current_win() ~= term_win then
+    if vim.api.nvim_get_current_win() == term_win then
+      term_height = vim.api.nvim_win_get_height(term_win)  
+      vim.api.nvim_win_close(term_win, true)
+      term_win = nil
+    else
       vim.api.nvim_set_current_win(term_win)
-      return
     end
-    vim.api.nvim_win_close(term_win, true)
-    term_win = nil
     return
   end
 
@@ -16,9 +24,14 @@ function ToggleTerminal()
     term_buf = vim.api.nvim_create_buf(false, true)
   end
 
-  local h = math.floor(vim.o.lines / 3)
+  local h = term_height or math.floor(vim.o.lines / 3)
+  h = math.min(h, vim.o.lines - 2)
+
+  local row = vim.o.lines - h - 1
+
   term_win = vim.api.nvim_open_win(term_buf, true, {
     relative = "editor",
+    row      = row,
     col      = 0,
     width    = vim.o.columns,
     height   = h,
