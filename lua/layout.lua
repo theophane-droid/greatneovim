4c22c5e3858ad62e584ba5beac536149f1880405
theophane-droid <theodesile@gmail.com>
2025-11-13 11:09:46 +0100

    window(fix zoom)

--- lua/layout.lua
+++ lua/layout.lua
@@ -1,22 +1,42 @@
-local zoom_tab = nil
+local zoom_state = {
+  active = false,
+  origin_tab = nil,
+  zoom_tab   = nil,
+}
 
 local function toggle_zoom()
-  if not zoom_tab then
-    vim.cmd("tab split")
-    zoom_tab = vim.api.nvim_get_current_tabpage()
+  local curtab = vim.api.nvim_get_current_tabpage()
+
+  if not zoom_state.active then
+    zoom_state.origin_tab = curtab
+
+    vim.cmd("tab split")  
+    zoom_state.zoom_tab = vim.api.nvim_get_current_tabpage()
+    zoom_state.active = true
     return
   end
 
-  if vim.api.nvim_get_current_tabpage() ~= zoom_tab then
-    vim.api.nvim_set_current_tabpage(zoom_tab)
-  end
+  if zoom_state.active then
+    if zoom_state.zoom_tab and vim.api.nvim_tabpage_is_valid(zoom_state.zoom_tab) then
+      vim.api.nvim_set_current_tabpage(zoom_state.zoom_tab)
+      vim.cmd("tabclose")
+    end
 
-  vim.cmd("tabclose")
-  zoom_tab = nil
+    if zoom_state.origin_tab and vim.api.nvim_tabpage_is_valid(zoom_state.origin_tab) then
+      vim.api.nvim_set_current_tabpage(zoom_state.origin_tab)
+    end
+
+    zoom_state.active = false
+    zoom_state.origin_tab = nil
+    zoom_state.zoom_tab   = nil
+  end
 end
 
 local function is_zoomed()
-  return zoom_tab ~= nil and vim.api.nvim_get_current_tabpage() == zoom_tab
+  return zoom_state.active
+     and zoom_state.zoom_tab
+     and vim.api.nvim_tabpage_is_valid(zoom_state.zoom_tab)
+     and vim.api.nvim_get_current_tabpage() == zoom_state.zoom_tab
 end
 
 return {
